cd79e5664c7068d1b2f5f45641a9b46f314f27f4
Kevin Koo <kevin.koo@g.skku.edu>
2021-09-04 09:23:47 +0900

    common header

---
+++ common.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// Lecture 01
+int bool_test();
+int comments();
+void var_test();
+int sizeof_test();
+int raw_string();
+void auto_test();
+void io_test();
+void constant();
+void enum_test();
+void enum_test2();
+void math_test();
+
+// Lecture 02
+void static_cast_test();
+void static_cast_test2();
+void const_cast_test();
+int reinterpret_cast_test();
